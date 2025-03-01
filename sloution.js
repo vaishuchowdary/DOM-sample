0390566b9c3427b66403777951e63693ee694bef
vaishuchowdary <vaishuchowdary039@gmail.com>
2025-03-01 13:21:08 +0530

    Add files via upload

---
+++ sloution.js
@@ -0,0 +1,15 @@
+var stat =document.querySelector("h5");
+var addFriend =document.querySelector("#add");
+var unFriend =document.querySelector("#remove");
+
+
+
+addFriend.addEventListener("click",function(){
+    stat.innerHTML="Friends";
+    stat.style.color="black"
+});
+unFriend.addEventListener("click",function(){
+    stat.innerHTML="Stranger";
+    stat.style.color="skyblue"
+
+});
